526611f0013bc8682e258538deb7de822bb2a897
Daniel Cook <danielecook@gmail.com>
2016-02-21 19:00:53 +0000

    updated

--- poll.py
+++ poll.py
@@ -5,32 +5,51 @@ from apiclient import discovery
 from oauth2client import client as oauth2client
 from subprocess import check_output
 from iron_mq import *
+from models import *
+
+
+# Get instance information
+#import requests
+#metadata_server = "http://metadata/computeMetadata/v1/instance/"
+#metadata_flavor = {'Metadata-Flavor' : 'Google'}
+#gce_id = requests.get(metadata_server + 'id', headers = metadata_flavor).text
+#gce_name = requests.get(metadata_server + 'hostname', headers = metadata_flavor).text
+#
+#worker_status.get_or_create(machine_id = gce_id, machine_name = gce_name)
 
 queue = IronMQ().queue("cegwas-map")
 
 while True:
 
-    resp = queue.get()["messages"]
+    resp = queue.reserve(timeout=3600)["messages"]
 
     if len(resp) > 0:
         message = resp[0]
-        print message["id"]
 
         # Acknowledge reciept of message
         args = message["body"]
         data = json.loads(args)
-        report_slug = data["report_info"]["report_slug"]
-        trait_name = data["trait_name"]
+        report_slug = data["report_slug"]
+        trait_slug = data["trait_slug"]
+        report_id = report.get(report_slug = report_slug).id
         # Run workflow
-        comm = """Rscript -e "library(knitr); knit('report.Rmd')" '{args}'""".format(args = args)
-        check_output(comm, shell = True)
+        comm = """Rscript -e "rmarkdown::render('report.Rmd')" '{args}'""".format(args = args)
+        try:
+            print comm
+            check_output(comm, shell = True)
 
-        # Upload results
-        upload1 = """gsutil cp report.html gs://cendr/{report_slug}/{trait_name}/report.html""".format(**locals())
-        check_output(upload1, shell = True)
-        upload2 = """gsutil cp -r figures gs://cendr/{report_slug}/{trait_name}/""".format(**locals())
-        check_output(upload2, shell = True)
-        #queue.delete(message["id"], message["reservation_id"])
+            # Upload results
+            upload1 = """gsutil cp report.html gs://cendr/{report_slug}/{trait_slug}/report.html""".format(**locals())
+            check_output(upload1, shell = True)
+            upload2 = """gsutil cp -r figures gs://cendr/{report_slug}/{trait_slug}/""".format(**locals())
+            check_output(upload2, shell = True)
 
+            # Update status of report submission
+            trait.update(submission_complete=datetime.datetime.now(), status="complete").where(trait.report == report_id, trait.trait_slug == trait_slug).execute()
+        except:
+            trait.update(submission_complete=datetime.datetime.now(), status="error").where(trait.report == report_id, trait.trait_slug == trait_slug).execute()
+        queue.delete(message["id"], message["reservation_id"])
+
+        
 
 
